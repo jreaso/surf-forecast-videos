0dd5d830a9ce00597a6fe96594d1fbecc228d3f8
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-08-25 23:46:45 +0100

    refactored SurflineWrapper class in forecast.py

--- forecast.py
+++ forecast.py
@@ -6,84 +6,128 @@ from utils import to_snake_case
 
 class SurflineWrapper:
     """
-    A client for interacting with the Surfline API to fetch forecast data.
+    A wrapper for interacting with the Surfline API to fetch forecast data.
     """
     BASE_URL = "https://services.surfline.com/kbyg/spots/forecasts/"
 
     def __init__(self):
+        """
+        Initialize a new instance of SurflineWrapper.
+
+        This constructor initializes a session to be used for making API requests.
+        """
         self.session = requests.Session()
 
     def fetch_forecast(self, params: dict):
-        data = {}
-        for attr in ('', 'wave', 'wind', 'tides', 'weather'):
-            data[attr] = self._fetch_attr_response(params, attr)
+        """
+        Fetch the forecast data from the Surfline API.
 
-        return self._process_response(data)
+        This function fetches forecast data from the Surfline API based on the provided parameters, and includes
+        processing into a more consistent/usable format.
+
+        Args:
+            params (dict): Parameters to be included in the API request.
+
+        Returns:
+            dict: A dictionary containing processed surf forecast data with various categories
+                  such as 'meta', 'surf', 'swell', 'wind', 'tide', 'weather', and 'sunlight_times'.
+        """
+        response_data = {}
+        for attr in ('', 'wave', 'wind', 'tides', 'weather'):
+            response_data[attr] = self._fetch_attr_response(params, attr)
+
+        return self._process_response(response_data)
 
     def _fetch_attr_response(self, params: dict, forecast_attr: str) -> dict:
+        """
+        Fetch a specific forecast attribute response from the Surfline API.
+
+        Args:
+            params (dict): Parameters to be included in the API request.
+            forecast_attr (str): The attribute to fetch data for.
+
+        Returns:
+            dict: The response data from the specified forecast attribute.
+        """
         url = f"{self.BASE_URL}{forecast_attr}?{urlencode(params)}"
         response = requests.get(url)
         if response.status_code != 200:
             raise ValueError(f"Error: {response.status_code}\n{response.reason}")
         return json.loads(response.text)
 
-    def _process_response(self, data: dict) -> dict:
-        # Meta
-        forecast = {'meta': {
-            'spotId': data['']['spotId'],
-            'utcOffset': data['']['utcOffset'],
-            'units': data['']['units']
-        }, 'surf': [], 'swells': [], 'wind': [], 'tides': [], 'weather': []}
+    def _process_response(self, response_data: dict) -> dict:
+        """
+        Process and refactor the response data from Surfline's API.
 
-        # Surf and Swells
-        for wave_obs in data['wave']['data']['wave']:
-            surf_obs = {
+        This function takes the responses data from Surfline's API and refactors the structure to flatten it out and
+        change naming conventions to be snake case as well as changing particular names for consistency.
+
+        Args:
+            response_data (dict): The raw responses data from Surfline's API.
+
+        Returns:
+            dict: A dictionary containing processed surf forecast data with various categories
+                  such as 'meta', 'surf', 'swell', 'wind', 'tide', 'weather', and 'sunlight_times'.
+        """
+        # Initialize forecast_data dictionary with meta data filled
+        forecast_data = {'meta': {
+            'spot_id': response_data['']['spotId'],
+            'utc_offset': response_data['']['utcOffset'],
+            'units': response_data['']['units']
+        }, 'surf': [], 'swells': [], 'wind': [], 'tide': [], 'weather': []}
+
+        # Wave (Surf and Swells)
+        for wave_obs in response_data['wave']['data']['wave']:
+            # Surf
+            forecast_data['surf'].append({
                 'timestamp': wave_obs['timestamp'],
-                'min': wave_obs['surf']['min'],
-                'max': wave_obs['surf']['max'],
-                'optimalScore': wave_obs['surf']['optimalScore'],
-                'humanRelation': wave_obs['surf']['humanRelation'],
-                'rawMin': wave_obs['surf']['raw']['min'],
-                'rawMax': wave_obs['surf']['raw']['max']
-            }
+                **{
+                    to_snake_case(key): wave_obs['surf'][key]
+                    for key in ('min', 'max', 'optimalScore', 'humanRelation')
+                },
+                **{
+                    f"raw_{key}": wave_obs['surf']['raw'][key]
+                    for key in ('min', 'max')
+                }
+            })
 
-            swell_obs = {
-                'timestamp': wave_obs['timestamp'],
-                'swells': wave_obs['swells']
-            }
+            # Swell - flatten the swells into lists
+            forecast_data['swell'].append({
+                'timestamp': [entry['timestamp'] for entry in forecast_data['swells']],
+                **{
+                    to_snake_case(key): [[entry['swells'][i][key]
+                                          for i in range(len(forecast_data['swells'][0]['swells']))]
+                                         for entry in forecast_data['swells']]
+                    for key in forecast_data['swells'][0]['swells'][0]
+                }
+            })
 
-            forecast['surf'].append(surf_obs)
-            forecast['swells'].append(swell_obs)
+        # Wind, Tide and Weather
+        for new_key, key, attributes in (
+            ('wind', 'wind', ('timestamp', 'speed', 'direction', 'directionType', 'gust', 'optimalScore')),
+            ('tide', 'tides', ('timestamp', 'type', 'height')),
+            ('weather', 'weather', ('timestamp', 'temperature', 'condition', 'pressure'))
+        ):
+            for obs in response_data[key]['data'][key]:
+                forecast_data[new_key].append({
+                    to_snake_case(attr): obs[attr] for attr in attributes
+                })
 
-        # Wind
-        for obs in data['wind']['data']['wind']:
-            wind_obs = {x: obs[x] for x in ('timestamp', 'speed', 'direction', 'directionType', 'gust', 'optimalScore')}
-            forecast['wind'].append(wind_obs)
-
-        # Tides
-        for obs in data['tides']['data']['tides']:
-            tides_obs = {x: obs[x] for x in ('timestamp', 'type', 'height')}
-            forecast['tides'].append(tides_obs)
-
-        # Weather
-        for obs in data['weather']['data']['weather']:
-            weather_obs = {x: obs[x] for x in ('timestamp', 'temperature', 'condition', 'pressure')}
-            forecast['weather'].append(weather_obs)
-
-        # SunriseSunsetTimes
-        forecast['sunriseSunsetTimes'] = {
-            x: data['weather']['data']['sunlightTimes'][0][x] for x in ('midnight', 'dawn', 'sunrise', 'sunset', 'dusk')
+        # Sunlight Times
+        forecast_data['sunlight_times'] = {
+            key: response_data['weather']['data']['sunlightTimes'][0][key]
+            for key in ('midnight', 'dawn', 'sunrise', 'sunset', 'dusk')
         }
 
-        return forecast
+        return forecast_data
 
 class Forecast:
     def __init__(self, data: dict):
         self.data = data
 
         # Meta Attributes
-        self.spot_id = self.data['meta']['spotId']
-        self.utc_offset = self.data['meta']['utcOffset']
+        self.spot_id = self.data['meta']['spot_id']
+        self.utc_offset = self.data['meta']['utc_offset']
         self.timestamps = [entry["timestamp"] for entry in self.data['surf']]
 
 
