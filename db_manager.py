dd3319a763ad59392f40331ddba928a16e48092b
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-08-26 16:13:34 +0100

    added functionality for adding forecast data in DBManager

--- db_manager.py
+++ db_manager.py
@@ -4,15 +4,28 @@ from forecast import Forecast
 
 class DBManager:
     def __init__(self, db_name: str) -> None:
+        """
+        Initialize the DBManager with the given database name, sets parameter for foreign keys to be used and checks
+        database has the correct schema.
+
+        Args:
+            db_name (str): The name of the SQLite database.
+        """
         self.db_name = db_name
         self.conn = sqlite3.connect(db_name)
         self.conn.execute("PRAGMA foreign_keys = ON;")
         self.cursor = self.conn.cursor()
 
         if not self._check_schema():
-            self._create_tables()
+            self.create_tables()
 
     def _check_schema(self) -> bool:
+        """
+        Check if the required tables exist in the database schema.
+
+        Returns:
+            bool: True if all required tables exist, False otherwise.
+        """
         tables = ("forecasts", "forecast_swells", "surf_spots", "surf_cams", "cam_footage")
 
         # noinspection SqlDialectInspection,SqlNoDataSourceInspection
@@ -21,7 +34,7 @@ class DBManager:
 
         return all(table in db_tables for table in tables)
 
-    def _create_tables(self) -> None:
+    def create_tables(self) -> None:
         # noinspection SqlDialectInspection,SqlNoDataSourceInspection
         create_tables_query = """
             CREATE TABLE IF NOT EXISTS forecasts (
@@ -79,8 +92,7 @@ class DBManager:
             
             CREATE TABLE IF NOT EXISTS surf_spots (
                 spot_id TEXT PRIMARY KEY,
-                spot_name TEXT,
-                region TEXT
+                spot_name TEXT
             );
             
             CREATE TABLE IF NOT EXISTS surf_cams (
@@ -110,10 +122,77 @@ class DBManager:
         self.cursor.execute(create_tables_query)
 
     def insert_forecast(self, forecast: list):
-        # inserts a forecast object
-        # may need to use methods for processing and inserting swells separately
-        # may need to check and call methods for inserting spots etc
-        pass
+        for forecast_entry in forecast:
+            spot_id = forecast_entry['spot_id']
+            timestamp = forecast_entry['timestamp']
+
+            # Check if the surf spot exists in the database, if not, insert it
+            self._insert_surf_spot_if_not_exists(spot_id)
+
+            # Insert all forecast data except swell data into forecasts table
+            self._insert_into_forecasts_table(forecast_entry)
+
+            # Insert forecast swells into forecast_swells table
+            self._insert_into_forecast_swells_table(forecast_entry)
+
+            # Commit the transaction
+            self.conn.commit()
+
+    def _insert_surf_spot_if_not_exists(self, spot_id: str) -> None:
+        # noinspection SqlDialectInspection,SqlNoDataSourceInspection
+        select_query = "SELECT spot_id FROM surf_spots WHERE spot_id = ?"
+        existing_spot = self.cursor.execute(select_query, (spot_id,)).fetchone()
+
+        if not existing_spot:
+            # noinspection SqlDialectInspection,SqlNoDataSourceInspection
+            insert_query = "INSERT INTO surf_spots (spot_id) VALUES (?)"
+            self.cursor.execute(insert_query, (spot_id,))
+
+    def _insert_into_forecasts_table(self, forecast_entry: dict) -> None:
+        columns = [
+            'spot_id', 'forecast_timestamp', 'utc_offset', 'surf_min', 'surf_max',
+            'surf_optimal_score', 'surf_human_relation', 'surf_raw_min', 'surf_raw_max',
+            'wind_speed', 'wind_direction', 'wind_direction_type', 'wind_gust',
+            'wind_optimal_score', 'forecast_probability', 'tide_type', 'tide_height',
+            'weather_temperature', 'weather_condition', 'weather_pressure', 'is_light'
+        ]
+        # `forecast_timestamp` column (in forecasts table) is equivalent to `timestamp` key (in forecast dict)
+        keys = [col if col != 'forecast_timestamp' else 'timestamp' for col in columns]
+
+        values = [forecast_entry[key] for key in keys]
+
+        # noinspection SqlDialectInspection,SqlNoDataSourceInspection
+        insert_query = f"INSERT INTO forecasts ({', '.join(columns)}) VALUES ({', '.join(['?'] * len(keys))})"
+        self.cursor.execute(insert_query, values)
+
+    def _insert_into_forecast_swells_table(self, forecast_entry: dict) -> None:
+        n = len(forecast_entry['swell_height'])
+
+        columns = ('spot_id', 'forecast_timestamp', 'swell', 'height', 'period', 'impact', 'power', 'direction',
+                   'direction_min', 'optimal_score')
+
+        values = zip(
+            [forecast_entry['spot_id']] * n,
+            [forecast_entry['timestamp']] * n,
+            range(1, 7),
+            forecast_entry['swell_height'],
+            forecast_entry['swell_period'],
+            forecast_entry['swell_impact'],
+            forecast_entry['swell_power'],
+            forecast_entry['swell_direction'],
+            forecast_entry['swell_direction_min'],
+            forecast_entry['swell_optimal_score']
+        )
+
+        # noinspection SqlDialectInspection,SqlNoDataSourceInspection
+        insert_query = f"INSERT INTO forecast_swells ({', '.join(columns)}) VALUES ({', '.join(['?'] * len(columns))})"
+        self.cursor.executemany(insert_query, values)
+
+    def insert_surf_spot(self, surf_spot: tuple) -> None:
+        # noinspection SqlDialectInspection,SqlNoDataSourceInspection
+        insert_query = "INSERT INTO surf_spots (spot_id, spot_name) VALUES (?, ?)"
+        self.cursor.execute(insert_query, surf_spot)
+        self.conn.commit()
 
     def insert_cam_footage(self, cam_footage):
         # inserts cam footage
