3e492a95eadd3547bd1c94de7980dea75507ae34
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-08-26 21:06:40 +0100

    testing and debugging

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ from forecast import Forecast, SurflineWrapper
 from db_manager import DBManager
 
 params = {
-    "spotId": '584204204e65fad6a77090cf',
+    "spotId": '5842041f4e65fad6a7708d0f',
     "days": 1,
     "intervalHours": 1,
 }
@@ -12,5 +12,9 @@ forecast = Forecast(forecast_data)
 
 db_client = DBManager('test')
 
+db_client.insert_surf_spot(('5842041f4e65fad6a7708d0f', 'J-Bay'))
+
 db_client.insert_forecast(forecast)
 db_client.close_connection()
+
+print(forecast.to_dataframe()['tide_height'])
