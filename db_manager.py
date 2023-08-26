c3d103e9e32b288ea4c3389ba2aefa1b9c32b6de
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-08-26 13:23:23 +0100

    setup structure of DBManager

--- db_manager.py
+++ db_manager.py
@@ -1,2 +1,34 @@
+import sqlite3
+from forecast import Forecast
+
+
 class DBManager:
-    pass
+    def __init__(self, db_name: str) -> None:
+        self.db_name = db_name
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def _init_database(self):
+        # check tables exist
+        pass
+
+    def _check_schema(self):
+        # checks table exists
+        pass
+
+    def _create_tables(self):
+        # creates tables
+        pass
+
+    def insert_forecast(self, forecast: list):
+        # inserts a forecast object
+        # may need to use methods for processing and inserting swells separately
+        # may need to check and call methods for inserting spots etc
+        pass
+
+    def insert_cam_footage(self, cam_footage):
+        # inserts cam footage
+        pass
+
+    def close_connection(self):
+        self.conn.close()
