83eadd4c6a9678e8808b4ecdc248028ccdce9497
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-08-31 12:10:30 +0100

    eddited script so can run headless and only log in once for multiple cameras

--- rewind_clip_scraper.py
+++ rewind_clip_scraper.py
@@ -6,19 +6,21 @@ from selenium.webdriver.support.ui import Select
 import undetected_chromedriver as uc  # avoids cloudflare bot preventions
 
 
-def fetch_rewind_links(spot_rewind_extension: str) -> list:
+def fetch_rewind_links(spot_rewind_extensions: list, headless=False) -> list:
     """
     Semi-automated (may require CAPTCHA solved by human) Selenium based web scraper to log in to surfline and parse the
     rewinds page for a surf spot, saving the urls to the videos on the cdn server.
 
-    :param spot_rewind_extension: extension on the base url for the rewind clips page for a certain surf spot.
+    :param spot_rewind_extensions: list of extensions on the base url for the rewind clips page for a certain surf spot.
+    :param headless: whether to run browser headless or not.
     :return: rewind_clip_urls, a list of urls with links to the cdn server for downloading clips.
     """
     # Initialize Selenium Driver
     options = uc.ChromeOptions()
-    # Headless Option - avoid as may need to intervene for CAPTCHA
-    # options.headless = True
-    # options.add_argument('--headless')
+    if headless:
+        # Headless Option may throw error for CAPTCHA
+        options.headless = True
+        options.add_argument('--headless')
     driver = uc.Chrome(options=options)
 
     wait = WebDriverWait(driver, 20)  # wait instance to wait for elements to load
@@ -53,51 +55,52 @@ def fetch_rewind_links(spot_rewind_extension: str) -> list:
     print("Logged In")
 
     # Redirect to Rewinds Page
-    rewind_url = f'https://www.surfline.com/surf-cams/{spot_rewind_extension}'
-    driver.get(rewind_url)
+    for spot_rewind_extension in spot_rewind_extensions:
+        rewind_url = f'https://www.surfline.com/surf-cams/{spot_rewind_extension}'
+        driver.get(rewind_url)
 
-    wait.until(EC.presence_of_element_located((By.ID, "sl-rewind-player")))  # wait until page loaded
+        wait.until(EC.presence_of_element_located((By.ID, "sl-rewind-player")))  # wait until page loaded
 
-    # Store Clip URLs From Page
-    rewind_clip_urls = []  # initialize an empty list to append to
+        # Store Clip URLs From Page
+        rewind_clip_urls = []  # initialize an empty list to append to
 
-    # Cycle Through Days
-    select_element = driver.find_element(By.ID, 'sl-cam-rewind-date')  # Locate the <select> element
+        # Cycle Through Days
+        select_element = driver.find_element(By.ID, 'sl-cam-rewind-date')  # Locate the <select> element
 
-    # Create a Select object to interact with the <select> element
-    select = Select(select_element)
-
-    # Get the options before interacting
-    option_values = [option.get_attribute("value") for option in select.options]
-
-    for option_value in option_values:
-        # Re-find the select element
-        select_element = driver.find_element(By.ID, 'sl-cam-rewind-date')
+        # Create a Select object to interact with the <select> element
         select = Select(select_element)
 
-        select.select_by_value(option_value)  # select the option
+        # Get the options before interacting
+        option_values = [option.get_attribute("value") for option in select.options]
 
-        wait.until(EC.visibility_of_element_located((By.ID, "sl-rewind-player")))  # wait until page loaded
+        for option_value in option_values:
+            # Re-find the select element
+            select_element = driver.find_element(By.ID, 'sl-cam-rewind-date')
+            select = Select(select_element)
 
-        # Cycle Through Clips
-        rewind_clips_container = driver.find_elements(By.CLASS_NAME, "CameraClips_cameraClips__KFOUH")[0]
-        rewind_clip_buttons = rewind_clips_container.find_elements(By.CLASS_NAME, "CameraClips_cameraClip__Wh_DL")
+            select.select_by_value(option_value)  # select the option
 
-        # Loop over the clips and click the button for each clip
-        for button in rewind_clip_buttons:
-            button.click()
+            wait.until(EC.visibility_of_element_located((By.ID, "sl-rewind-player")))  # wait until page loaded
 
-            # wait until link div is loaded
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "CamRewind_camRewindDownloadBar__hc24X")))
+            # Cycle Through Clips
+            rewind_clips_container = driver.find_elements(By.CLASS_NAME, "CameraClips_cameraClips__KFOUH")[0]
+            rewind_clip_buttons = rewind_clips_container.find_elements(By.CLASS_NAME, "CameraClips_cameraClip__Wh_DL")
 
-            # Select Link
-            element = driver.find_element(By.CLASS_NAME, "CamRewind_camRewindDownloadBar__hc24X")
-            link = element.find_element(By.TAG_NAME, "a").get_attribute("href")
+            # Loop over the clips and click the button for each clip
+            for button in rewind_clip_buttons:
+                button.click()
 
-            rewind_clip_urls.append(link)  # append link
+                # wait until link div is loaded
+                wait.until(EC.presence_of_element_located((By.CLASS_NAME, "CamRewind_camRewindDownloadBar__hc24X")))
 
-            # wait until page loaded fully before moving on
-            wait.until(EC.visibility_of_element_located((By.ID, "sl-rewind-player")))
+                # Select Link
+                element = driver.find_element(By.CLASS_NAME, "CamRewind_camRewindDownloadBar__hc24X")
+                link = element.find_element(By.TAG_NAME, "a").get_attribute("href")
+
+                rewind_clip_urls.append(link)  # append link
+
+                # wait until page loaded fully before moving on
+                wait.until(EC.visibility_of_element_located((By.ID, "sl-rewind-player")))
 
     driver.quit()
 
