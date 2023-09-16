9208e2bc545d36744f574156b523a4fda1145ce1
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-16 15:08:08 +0100

    modifed which spots to delete

--- scripts/modify_db.py
+++ scripts/modify_db.py
@@ -1,5 +1,6 @@
 from src.db_manager import DBManager
 
+
 def remove_all_cam_videos(db_manager: DBManager) -> None:
     """
     Removes all entries in cam_videos table.
@@ -10,24 +11,34 @@ def remove_all_cam_videos(db_manager: DBManager) -> None:
 
     db_manager.run_query(query)
 
+
 def remove_cam_forecasts(db_manager: DBManager) -> None:
     """
-    Removes all entries from `sunlight_times`, `surf_cams`, `surf_spots`, `forecasts` and `forecast_swells` tables where the spot_id is "5842041f4e65fad6a7708d0f" (J-Bay) or "5842041f4e65fad6a7708bc3" (Supertubos).
+    Removes all entries from `sunlight_times`, `surf_cams`, `surf_spots`, `forecasts` and `forecast_swells` tables where the spot_id is "5842041f4e65fad6a7708d0f" (J-Bay).
+
+    The rest of the spot forecasts are left.
 
     :param db_manager: An instance of the DBManager class for database operations.
     """
     queries = (
-        "DELETE FROM sunlight_times WHERE spot_id IN ('5842041f4e65fad6a7708d0f', '5842041f4e65fad6a7708bc3')",
-        "DELETE FROM surf_cams WHERE spot_id IN ('5842041f4e65fad6a7708d0f', '5842041f4e65fad6a7708bc3')",
-        "DELETE FROM forecasts WHERE spot_id IN ('5842041f4e65fad6a7708d0f', '5842041f4e65fad6a7708bc3')",
-        "DELETE FROM forecast_swells WHERE spot_id IN ('5842041f4e65fad6a7708d0f', '5842041f4e65fad6a7708bc3')",
+        "DELETE FROM sunlight_times WHERE spot_id = '5842041f4e65fad6a7708d0f'",
+        "DELETE FROM surf_cams WHERE spot_id = '5842041f4e65fad6a7708d0f'",
+        "DELETE FROM forecast_swells WHERE spot_id = '5842041f4e65fad6a7708d0f'",
+        "DELETE FROM forecasts WHERE spot_id = '5842041f4e65fad6a7708d0f'",
     )
 
     for query in queries:
         db_manager.run_query(query, commit=False)
 
-    query = """
-        DELETE FROM surf_spots WHERE spot_id IN ('5842041f4e65fad6a7708d0f', '5842041f4e65fad6a7708bc3')
-    """
+    query = "DELETE FROM surf_spots WHERE spot_id = '5842041f4e65fad6a7708d0f'"
 
     db_manager.run_query(query, commit=True)
+
+
+if __name__ == "__main__":
+    surf_db_manager = DBManager('SurfForecastDB')
+
+    remove_all_cam_videos(surf_db_manager)
+    remove_cam_forecasts(surf_db_manager)
+
+    surf_db_manager.close_connection()
